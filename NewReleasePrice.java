d5ab451f9f9a6a698b70be2fd4f2c980776ffced
wendervitor <wendernogueira99@gmail.com>
2021-03-09 21:35:58 -0300

    Commit 13

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -1,5 +1,9 @@
 public class NewReleasePrice extends Price {
-   public int getPriceCode() {
-       return Movie.NEW_RELEASE;
-   }
+    public int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+    
+    public double getCharge(int daysRented){
+        return daysRented * 3;
+    } 
 }
