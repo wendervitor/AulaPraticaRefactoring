787c150249b48f6d0f8e2d89ad8cce9b2084b524
wendervitor <wendernogueira99@gmail.com>
2021-03-09 21:08:46 -0300

    Commit 9

--- Movie.java
+++ Movie.java
@@ -1,26 +1,46 @@
 public class Movie {
 
-  public static final int  CHILDRENS = 2;
-  public static final int  REGULAR = 0;
-  public static final int  NEW_RELEASE = 1;
+    public static final int  CHILDRENS = 2;
+    public static final int  REGULAR = 0;
+    public static final int  NEW_RELEASE = 1;
 
-  private String _title;
-  private int _priceCode;
+    private String _title;
+    private int _priceCode;
 
-  public Movie(String title, int priceCode) {
-      _title = title;
-      _priceCode = priceCode;
-  }
+    public Movie(String title, int priceCode) {
+        _title = title;
+        _priceCode = priceCode;
+    }
 
-  public int getPriceCode() {
-      return _priceCode;
-  }
+    public int getPriceCode() {
+        return _priceCode;
+    }
 
-  public void setPriceCode(int arg) {
-      _priceCode = arg;
-  }
+    public void setPriceCode(int arg) {
+        _priceCode = arg;
+    }
 
-  public String getTitle (){
-      return _title;
-  }
+    public String getTitle (){
+        return _title;
+    }
+    public double getCharge(int daysRented){
+        double thisAmount = 0;
+        //determine amounts for each line
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+            break;
+                case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+            break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                thisAmount += (daysRented - 3) * 1.5;
+            break;
+        }
+        return thisAmount;
+    }
 }
