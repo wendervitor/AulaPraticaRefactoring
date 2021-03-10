dfd690be3db5ab8d4f307fe92961fa54ea3584f4
wendervitor <wendernogueira99@gmail.com>
2021-03-09 21:43:00 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -1,10 +1,7 @@
 public abstract class Price {
    public abstract int getPriceCode();
    public abstract double getCharge(int daysRented);
-   public int getFrequentRenterPoints(int daysRented){
-      // add (or not) bonus for a two day new release rental
-      if ((getPriceCode() == Movie.NEW_RELEASE) &&
-         daysRented > 1) return 2;
+   public int getFrequentRenterPoints(int daysRented) {
       return 1;
    }
 
